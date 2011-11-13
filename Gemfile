a12cfa1e9ffa6ffcea236ee705a561862fda47bf
Will Leinweber <will@bitfission.com>
2011-11-12 17:55:51 -0800

    rotate keys

--- Gemfile
+++ Gemfile
@@ -5,9 +5,10 @@ gem 'haml'
 gem 'thin'
 gem 'sequel'
 gem 'pg'
+gem 'rest-client'
 
 group :test do
   gem 'rspec'
-  gem 'database_cleaner', :git => 'git://github.com/bmabey/database_cleaner.git'
+  gem 'database_cleaner'
 end
 
--- app/user.rb
+++ app/user.rb
@@ -1,3 +1,30 @@
-class User < Sequel::Model
+require 'rest_client'
+require 'json'
 
+class User < Sequel::Model
+  def rotate_keys!
+    write_keys(SecureKey.generate, get_current_key)
+  end
+
+  def write_keys(new_key, old_key)
+   keys = {"SECURE_KEY" => new_key, "SECURE_KEY_OLD" => old_key}
+   RestClient.put(heroku_url, {
+        :config => keys
+      }.to_json,
+      :content_type => :json,
+      :accept       => :json
+    )
+  end
+
+  def get_current_key
+    response = JSON.parse(RestClient.get(heroku_url))
+    response['config']['SECURE_KEY']
+  end
+
+  def heroku_url
+    url = URI.parse(callback_url)
+    url.user = ENV["HEROKU_USERNAME"]
+    url.password = ENV["HEROKU_PASSWORD"]
+    return url.to_s
+  end
 end
