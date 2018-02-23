44224087654849279d929ff08f5f688c8298fbba
Dan Croak <dan@statusok.com>
2018-02-22 21:42:26 -0800

    ensure hex values 64 characters in length
    Previous method sometimes provides 63 chars.
    Guaranteeing 64 chars allows consumers to use hex decoding methods.

--- spec/secure_key_spec.rb
+++ spec/secure_key_spec.rb
@@ -4,4 +4,10 @@ describe SecureKey, ".generate" do
   it 'makes different keys' do
     SecureKey.generate.should_not == SecureKey.generate
   end
+
+  it 'generates hex-encodable keys reliably 64 characters in length' do
+    100.times do
+      SecureKey.generate.length.should == 64
+    end
+  end
 end
