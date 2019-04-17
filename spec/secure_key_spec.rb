cafeb4aaaa986e562e66f030f507a672708ea242
Will Leinweber <will@bitfission.com>
2019-04-17 18:54:32 +0200

    update everything

--- spec/secure_key_spec.rb
+++ spec/secure_key_spec.rb
@@ -1,11 +1,11 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe SecureKey, ".generate" do
-  it 'makes different keys' do
+  it "makes different keys" do
     SecureKey.generate.should_not == SecureKey.generate
   end
 
-  it 'generates hex-encodable keys reliably 64 characters in length' do
+  it "generates hex-encodable keys reliably 64 characters in length" do
     100.times do
       SecureKey.generate.length.should == 64
     end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
-require 'rspec'
-require './app/secure_key'
-require 'webmock/rspec'
+require "rspec"
+require "./app/secure_key"
+require "webmock/rspec"
 WebMock.disable_net_connect!
 
 RSpec.configure do |config|
-  config.expect_with(:rspec) { |c| c.syntax = :should }
+  config.expect_with(:rspec) { |c| c.syntax = %i[expect should] }
   def clean!
     db = Sequel::Model.db
     (db.tables - [:schema_info, :schema_migrations]).each do |table|
@@ -13,12 +13,11 @@ RSpec.configure do |config|
   end
   config.before(:suite) { clean! }
   config.before(:each) do
-    @start = Sequel::Model.db['select txid_current()'].first[:txid_current]
+    @start = Sequel::Model.db["select txid_current()"].first[:txid_current]
   end
   config.after(:each) do
-    if (Sequel::Model.db['select txid_current()'].first[:txid_current] - @start) > 1
+    if (Sequel::Model.db["select txid_current()"].first[:txid_current] - @start) > 1
       clean!
     end
   end
 end
-
