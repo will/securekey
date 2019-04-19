cafebb796b93d5fe17b964c72a52b92ab708edec
Will Leinweber <will@bitfission.com>
2019-04-19 18:09:26 +0200

    store uuid on provision

--- app/web.rb
+++ app/web.rb
@@ -1,75 +1,64 @@
-require './app/secure_key'
-require 'sinatra/base'
-require 'json'
-require 'rdiscount'
+require "./app/secure_key"
+require "sinatra/base"
+require "json"
+require "rdiscount"
 
 class App < Sinatra::Base
-  use Rack::Session::Cookie, secret: ENV['SSO_SALT']
   enable :inline_templates
-  set :environment, ENV['RACK_ENV']
+  set :environment, ENV["RACK_ENV"]
 
   helpers do
     def refuse_provision(reason)
-      throw(:halt, [422, {:message => reason}.to_json])
+      throw(:halt, [422, {message: reason}.to_json])
     end
 
     def protected!
       unless authorized?
-        response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
+        response["WWW-Authenticate"] = %(Basic realm="Restricted Area")
         throw(:halt, [401, "Not authorized\n"])
       end
     end
 
     def authorized?
-      @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+      @auth ||= Rack::Auth::Basic::Request.new(request.env)
       @auth.provided? && @auth.basic? && @auth.credentials &&
-      @auth.credentials == [ENV['HEROKU_USERNAME'], ENV['HEROKU_PASSWORD']]
+        @auth.credentials == [ENV["HEROKU_USERNAME"], ENV["HEROKU_PASSWORD"]]
     end
   end
 
   get "/" do
-    markdown File.read('./README.md'), :layout_engine => :erb
+    markdown File.read("./README.md"), layout_engine: :erb
   end
 
   # sso sign in
   get "/heroku/resources/:id" do
     halt 501
-    #pre_token = params[:id] + ':' + ENV['SSO_SALT'] + ':' + params[:timestamp]
-    #token = Digest::SHA1.hexdigest(pre_token).to_s
-    #halt 403 if token != params[:token]
-    #halt 403 if params[:timestamp].to_i < (Time.now - 2*60).to_i
-
-    #account = true #User.get(params[:id])
-    #halt 404 unless account
-
-    #session[:heroku_sso] = params['nav-data']
-    #response.set_cookie('heroku-nav-data', value: params['nav-data'])
-    #haml :index
   end
 
   # provision
-  post '/heroku/resources' do
+  post "/heroku/resources" do
     protected!
     params = JSON.parse(request.body.read)
-    p params if ENV['DEBUG']
+    p params if ENV["DEBUG"]
 
-    if params['plan'] == 'test' && ENV['HEROKU_USERNAME']=='securekey' # only production
+    if params["plan"] == "test" && ENV["HEROKU_USERNAME"] == "securekey" # only production
       refuse_provision("test plan deprecated, please use fortnightly")
     end
 
-    u = User.create logplex_token: params['logplex_token'],
-                     callback_url: params['callback_url'],
-                        heroku_id: params['heroku_id'],
-                             plan: params['plan']
+    u = User.create logplex_token: params["logplex_token"],
+                     callback_url: params["callback_url"],
+                        heroku_id: params["heroku_id"],
+                             plan: params["plan"],
+                             uuid: params["uuid"]
 
     u.update_next_rotation_time!
     status 201
-    inital_keys = [SecureKey.generate, SecureKey.generate].join(',')
-    {id: u.id, value: inital_keys, config: {'KEY' => inital_keys} }.to_json
+    inital_keys = [SecureKey.generate, SecureKey.generate].join(",")
+    {id: u.id, value: inital_keys, config: {"KEY" => inital_keys}}.to_json
   end
 
   # deprovision
-  delete '/heroku/resources/:id' do
+  delete "/heroku/resources/:id" do
     protected!
     u = User[params[:id].to_i]
     if u
@@ -81,11 +70,11 @@ class App < Sinatra::Base
   end
 
   # plan change
-  put '/heroku/resources/:id' do
+  put "/heroku/resources/:id" do
     protected!
     body = JSON.parse(request.body.read)
     u = User[params[:id].to_i]
-    if u && u.update(plan: body['plan'])
+    if u&.update(plan: body["plan"])
       "ok"
     else
       status 404
